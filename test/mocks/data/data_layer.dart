be3ae7e75a2f2ed4bfe3a44e09d9e0df7bd56f27
darkwolf999 <darkwolf999@mail.ru>
2023-07-06 00:30:53 +0300

    one more unit test

--- test/mocks/data/data_layer.dart
+++ test/mocks/data/data_layer.dart
@@ -2,10 +2,7 @@ import 'package:mockito/annotations.dart';
 import 'package:todo/data/api/database_tasks_api.dart';
 import 'package:todo/data/api/network_tasks_api.dart';
 import 'package:todo/data/api/revision_provider.dart';
-import 'package:todo/domain/repository/tasks_repository.dart';
 import 'package:todo/helpers/network_checker/network_checker.dart';
-
-import 'data_layer.mocks.dart';
 
 @GenerateMocks([
   NetworkTasksApi,
