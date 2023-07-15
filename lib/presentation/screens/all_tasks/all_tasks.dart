74b33eec13e62fab61eff11244a5f565f8139fbf
darkwolf999 <darkwolf999@mail.ru>
2023-07-15 04:40:13 +0300

    delete test Exception

--- lib/presentation/screens/all_tasks/all_tasks.dart
+++ lib/presentation/screens/all_tasks/all_tasks.dart
@@ -141,7 +141,6 @@ class AllTasksScreenContent extends StatelessWidget {
           FloatingActionButton(
             onPressed: () {
               addNewTask(scrollController, router);
-              throw Exception();
             },
             backgroundColor: const Color(Constants.lightColorBlue),
             tooltip: LocaleKeys.addTask.tr(), //'Добавить дело',
